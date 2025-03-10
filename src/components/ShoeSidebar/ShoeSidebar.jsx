801e6df40cfca7247c67ecb994040c46e9d1e625
Stephen Beattie <beattie19@hotmail.co.uk>
2025-03-10 22:36:19 +1100

    Attempt to get a custom hover style - couldn't figure out how to get the link to sit in front of the brackets I made

--- src/components/ShoeSidebar/ShoeSidebar.jsx
+++ src/components/ShoeSidebar/ShoeSidebar.jsx
@@ -6,7 +6,7 @@ import { WEIGHTS } from '../../constants';
 const Sidebar = () => {
   return (
     <Wrapper>
-      <Link href="/lifestyle">Lifestyle</Link>
+      <LinkWrapper><Link href="/lifestyle">Lifestyle<Bracket><div>[</div><div>]</div></Bracket></Link></LinkWrapper>  
       <Link href="/jordan">Jordan</Link>
       <ActiveLink href="/running">Running</ActiveLink>
       <Link href="/basketball">Basketball</Link>
@@ -25,6 +25,10 @@ const Sidebar = () => {
 
 const Wrapper = styled.aside``;
 
+ const Bracket = styled.div`
+   display: flex;
+ `
+
 const Link = styled.a`
   display: block;
   text-decoration: none;
@@ -36,5 +40,49 @@ const Link = styled.a`
 const ActiveLink = styled(Link)`
   color: var(--color-primary);
 `;
+
+const LinkWrapper = styled.div`
+  display: flex;
+
+  ${Bracket} {
+    position: relative;
+    left: 0;
+    margin-right: 8px;
+    align-items: center;
+    width: 130%;
+    margin-left: -15%;
+    justify-content: center;
+
+    :first-child {
+      transition: transform 350ms ease-in, opacity 150ms;
+
+        transform: translateX(0);
+        opacity: 0.5;
+      }
+
+    :last-child {
+      transition: transform 350ms ease-in, opacity 150ms;
+
+      transform: translateX(0);
+      opacity: 0.5;
+    }
+    
+    &:hover {
+      :first-child {
+        transition: transform 350ms ease-in, opacity 150ms;
+
+        transform: translateX(-50px);
+        opacity: 1;
+      }
+
+      :last-child {
+        transition: transform 350ms ease-in, opacity 150ms;
+
+        transform: translateX(50px);
+        opacity: 1;
+      }
+  }
+}
+`
 
 export default Sidebar;
